a941ee373f4dccb854a8df226a21d967e2bdedee
Rogelio Meza <rogelio.meza.t@gmail.com>
2014-11-19 13:00:45 -0300

    add cover to book (with paperclip)

--- app/models/book.rb
+++ app/models/book.rb
@@ -18,9 +18,13 @@
 #
 
 class Book < ActiveRecord::Base
-  
+
   validates :title, :author, :editorial, :presence => true
   validates :edition, :publication_year, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
-  
-  
+
+  #paperclip
+  has_attached_file :cover, :styles => { :medium => "300x300>", :thumb => "100x100>", :mini => "30x30>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+
 end
